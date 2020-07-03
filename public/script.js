36adab094e09207bc5a5de53deac6547534c9feb
Felipe <felipedemelopontes@gmail.com>
2020-07-03 22:57:40 +0200

    feat: limit photos

--- public/script.js
+++ public/script.js
@@ -45,13 +45,28 @@ const PhotosUpload = {
         PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
     hasLimit(event) {
-        const { uploadLimit, input: fileList } = PhotosUpload
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
 
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos!`)
             event.preventDefault()
             return true
         }
+
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo")
+            photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if (totalPhotos > uploadLimit) {
+            alert("Atingiu o limite")
+            event.preventDefault()
+            return true
+        }
+
         return false
     },
     getAllFiles() {
