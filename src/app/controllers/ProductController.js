d11d3b1a239004e3b1470469638a61d5b839d7e8
Felipe <felipedemelopontes@gmail.com>
2020-07-06 16:58:00 +0200

    feat: refresh preview photos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -74,6 +74,25 @@ module.exports = {
             }
         }
 
+        if(req.files.length != 0) {
+            const newFilesPromise = req.files.map(file =>
+                File.create({...file, product_id: req.body.id}))
+
+                await Promise.all(newFilesPromise)
+        }
+
+        if (req.body.removed_files) {
+
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
+
+
         req.body.price = req.body.price.replace(/\D/g, "")
 
         if(req.body.old_price != req.body.price) {
