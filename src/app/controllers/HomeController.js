3f25f8dc02c1d94f3eba02654a89be9c0a1eb552
Felipe <felipedemelopontes@gmail.com>
2020-08-24 11:06:55 +0200

    feat: home logic

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,8 +1,6 @@
 const {formatPrice} = require('../../lib/utils')
 
-const Category = require('../models/Category')
-const File = require('../models/File')
-
+const Product = require('../models/Product')
 
 module.exports = {
     async index(req, res) {
@@ -11,5 +9,22 @@ module.exports = {
 
         if (!products) return results.send("Products not found")
 
+        async function getImage(productId) {
+            let results = await Product.files(productId)
+            const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+
+            return files[0]
+        }
+
+        const productsPromise = products.map(async product => {
+            product.img = await getImage(product.id)
+            product.oldPrice = formatPrice(product.old_price)
+            product.price = formatPrice(product.price)
+            return product
+        }).filter((product, index) => index > 2 ? false : true);
+
+        const lastAdded = await Promise.all(productsPromise)
+
+        return res.render("home/index", { products: lastAdded })
     }
 }
